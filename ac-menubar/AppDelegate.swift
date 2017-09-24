0908241ba57b31c257d21ab0e6b52eec0eee5bd2
jae kaplan <me@jkap.io>
2017-09-24 13:51:21 -0700

    Initial Commit

---
+++ ac-menubar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ac-menubar
+//
+//  Created by jæ kaplan on 9/24/17.
+//  Copyright © 2017 jae kaplan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
